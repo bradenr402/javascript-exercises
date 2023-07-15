bc4ce7002518b1c6a8ff966d8e5a01c780dfbea4
Braden <108199884+bradenr402@users.noreply.github.com>
2023-07-15 12:24:55 -0400

    Exercise 03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(originalString) {
+    // split string into an array
+    // reverse the array
+    // join the array back into a string
+    // return the reversed string
 
+    originalString = originalString.split('');
+    let reversedString = originalString.reverse();
+    reversedString = reversedString.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
