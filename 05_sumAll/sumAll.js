32d054af4952e84bbd47eabf58e2f89888ee1aab
Braden <108199884+bradenr402@users.noreply.github.com>
2023-07-15 21:41:42 -0400

    Exercise 05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (typeof first!== 'number' || typeof second!== 'number') return 'ERROR';
 
+    let sum = 0;
+    let parametersArray = [first, second];
+    parametersArray.sort((a, b) => a - b);
+    
+    let start = parametersArray[0],
+        end = parametersArray[1];
+
+    if (start < 0 || end < 0) return 'ERROR';
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
