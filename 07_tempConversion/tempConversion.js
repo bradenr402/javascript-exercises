7dca8013c7afef44c6c0e572689fe66a2a4dd01d
Braden <108199884+bradenr402@users.noreply.github.com>
2023-07-15 22:12:11 -0400

    Exercise 07 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+    // (temp - 32) * 5/9
+    
+    let tempToCelsius = (fahrenheitTemp - 32) * 5/9;
+    return (Math.round(tempToCelsius * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+    // (temp * 9/5 + 32) 
+    
+    let tempToFahrenheit = (celsiusTemp * 9/5 + 32);
+    return (Math.round(tempToFahrenheit * 10) / 10);
 };
-
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit
 };
