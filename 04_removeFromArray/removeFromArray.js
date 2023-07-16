00db184f35ae4e45b3986d75790ea1bbed353d0e
Braden <108199884+bradenr402@users.noreply.github.com>
2023-07-15 21:26:14 -0400

    Exercise 04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,30 +1,10 @@
-const removeFromArray = function(array, index1, index2, index3, index4) {
-
-    let itemIndex1 = array.indexOf(index1);
-    let itemIndex2 = array.indexOf(index2);
-    let itemIndex3 = array.indexOf(index3);
-    let itemIndex4 = array.indexOf(index4);
-
-    let resultArray = array.slice(0, itemIndex1).concat(array.slice(itemIndex1 + 1));
-    if (index2 == undefined) {
-        return resultArray ;
+const removeFromArray = function(array, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        args[i] = array.indexOf(args[i]);
+        if (args[i] === -1) continue;
+        array.splice(args[i], 1);
     }
-
-    resultArray = resultArray.slice(0, itemIndex2).concat(resultArray.slice(itemIndex2 + 1));
-
-    if (index3 == undefined) {
-        return resultArray;
-    }
-
-    resultArray = resultArray.slice(0, itemIndex3).concat(resultArray.slice(itemIndex3 + 1));
-
-    if (index4 == undefined) {
-        return resultArray;
-    }
-
-    resultArray = [];
-
-    return resultArray;
+    return array;
 };
 
 // Do not edit below this line
