539e86f30e92c44dc0305f08a2d57bacf68976cc
Braden <108199884+bradenr402@users.noreply.github.com>
2023-07-15 21:50:43 -0400

    Exercise 06 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // divisible by 4
+    // not divisible by 400, unless divisible by 400
 
+    /* use an 'if' statement and '&&' 
+    to make sure all the conditions are 
+    met properly */
+
+    if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
