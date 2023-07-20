786e9578861c15162c1f938e656f01eb80f6f51f
Braden <108199884+bradenr402@users.noreply.github.com>
2023-07-20 14:18:58 -0400

    Exercise 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const processedString = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+    let reversedStr = processedString.split('').reverse().join('');
+    return processedString === reversedStr;
 };
 
 // Do not edit below this line
