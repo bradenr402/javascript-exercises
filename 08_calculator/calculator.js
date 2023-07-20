de38b9a47efafcd642e31eb89c207dab83a9599e
Braden <108199884+bradenr402@users.noreply.github.com>
2023-07-20 14:02:21 -0400

    Exercise 08 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function(a, b) {
+  return a - b;
 
 };
 
-const power = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const factorial = function() {
-	
+const multiply = function(...arr) {
+  return arr.reduce((product, current) => product * current, 1);
+};
+
+const power = function(base, exponent) {
+  let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result *= base;
+  }
+  return result;
+};
+
+const factorial = function(value) {
+  let result = 1;
+
+  if (value === 0) return 1;
+
+  for (let i = 1; i <= value; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
