e44467c5f0755fd63bdde23f3fc4a42717a3856f
Braden <108199884+bradenr402@users.noreply.github.com>
2023-07-20 14:43:13 -0400

    Exercise 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(fibNum) {
+    if (fibNum < 0) return 'OOPS';
 
+    let previous = 1;
+    let beforePrevious = 0;
+
+    for (let i = 2; i <= fibNum; i++) {
+        let current = previous + beforePrevious;
+        beforePrevious = previous;
+        previous = current;
+    }
+    return previous;
 };
 
 // Do not edit below this line
